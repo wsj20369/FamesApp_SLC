aef47372290100ddd5fdd0e273fc05e6f9c55d04
shujun <shujun20369@gmail.com>
2012-12-17 23:05:03 +0800

    Update version to 0.9.8

--- src/version.c
+++ src/version.c
@@ -25,7 +25,7 @@ char  ___AUTHOR[]    =      "Jun";
 #define VersionMajor         0
 #define VersionMinor         9
 
-#define VersionString       "0.9.7" /* 不可超过32个字节 */
+#define VersionString       "0.9.8" /* 不可超过32个字节 */
 
 INT16U get_version(void)
 {
